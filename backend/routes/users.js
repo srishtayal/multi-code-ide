54f8d8ba7b9a96b766ae13c6feae0fc05a5ad71b
Srishti Tayal <168971159+srishtayal@users.noreply.github.com>
2025-04-02 16:54:38 +0530

    fix

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 const { signUp, login } = require('../controllers/userController');
 var router = express.Router();
 
+router.post("/login", login);
 router.post("/signUp", signUp);  
 
 module.exports = router;
