c35ca500a1c72696b01a2c6f92a3a1fbf65eff74
Srishti Tayal <168971159+srishtayal@users.noreply.github.com>
2025-04-02 18:53:11 +0530

    Added userModel

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User"); // Make sure you have this model
+const User = require("../models/userModel"); // Make sure you have this model
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
