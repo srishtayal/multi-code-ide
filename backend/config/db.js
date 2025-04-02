f540b2c3f202039352130b21146b071972807e71
Srishti Tayal <168971159+srishtayal@users.noreply.github.com>
2025-04-02 16:48:25 +0530

    deploy fixes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,14 @@
 const mongoose = require("mongoose");
-require('dotenv').config();
-
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true, // These options are now unnecessary in Mongoose 6+
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host} | DB: ${conn.connection.name}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1); // Exit process with failure
   }
 };
 
